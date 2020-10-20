ab8c85a374b8dab21848ec3159a8087a1ca6770f
alves1609 <isabella.candido@aluno.faculdadeimpacta.com.br>
2020-10-19 21:27:52 -0300

    fibo

--- fibo.py
+++ fibo.py
@@ -2,25 +2,25 @@ import os
 from flask import Flask, jsonify, request
 from math import sqrt
 
-app = Flask(__name__)
+app = Flask(__fibo__)
 
 @app.route('/')
-def nao_entre_em_panico():
+def fibo():
    proximo = 1
    anterior = 0
    limite = 50
-   found = 0
+   f = 0
    resposta = "0,"
-   while (found < limite):
-       tmp = proximo
+   while (f < limite):
+       t = proximo
        proximo = proximo + anterior
-       anterior = tmp
-       found=found+1
+       anterior = t
+       f=f+1
        resposta+= str(proximo) + ","
 
 
    return resposta
 
-if __name__ == "__main__":
+if __fibo__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
